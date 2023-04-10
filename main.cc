ae81f49f96f0ea5d27d9bdcc1df01e41f4f19afd
connortbot <loiconnor8@gmail.com>
2023-04-10 04:45:57 -0400

    3.3 Peter Shirley

---
+++ main.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// COMPILE
+// g++ -std=c++11 -o renderer main.cc
+
+int main() {
+    const int image_width = 256;
+    const int image_height = 256;
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int j=image_height-1; j>=0; j--) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    std::cerr << "\nDone.\n";
+}
