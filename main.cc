124e0ff31cb7d6c9d4210796ab2ce5e3cad954cf
connortbot <loiconnor8@gmail.com>
2023-04-11 04:23:54 -0400

    5. Sending rays into scenes

--- main.cc
+++ main.cc
@@ -1,20 +1,41 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 
 // COMPILE
 // g++ -std=c++11 -o renderer main.cc
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y()+1.0);
+    return (1.0-t)*color(1.0,1.0,1.0)+t*color(0.5,0.7,1.0); //lerp formula (1.0-t)*start + t*endval
+}
+
 int main() {
-    const int image_width = 256;
-    const int image_height = 256;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0; // distance between camera and 'viewport'
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for (int j=image_height-1; j>=0; j--) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color(ray_color(r));
             write_color(std::cout, pixel_color);
         }
     }
