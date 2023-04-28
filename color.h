d83d572f2e1de891f6557d59e38102ff55831f75
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2023-04-27 18:47:15 -0700

    Fixed output-bug which was greyscaling image

--- color.h
+++ color.h
@@ -15,8 +15,8 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     g *= scale;
     b *= scale;
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
-        << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
-        << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << '\n';
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif
