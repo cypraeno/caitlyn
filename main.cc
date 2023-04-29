1370fa424469efbc3b866c2197cfd7e679157540
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2023-04-29 12:22:01 -0700

    Initial Implementation of Shirley-8

--- main.cc
+++ main.cc
@@ -11,10 +11,23 @@
 // COMPILE
 // g++ -std=c++11 -O2 -o my_renderer main.cc
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec)) {
-        return 0.5*(rec.normal + color(1,1,1));
+
+    // if exceed bounce limit, return black (no light)
+    if (depth <= 0) {
+        return color(0, 0, 0);
+    }
+
+    // 0.001 instead of 0 to correct for shadow acne
+    if (world.hit(r, 0.001, infinity, rec)) {
+        // 3 different diffusion options (uncomment desired one)
+
+        // point3 target = rec.p + rec.normal + random_in_unit_sphere();            // Rejection Method
+        point3 target = rec.p + rec.normal + random_unit_vector();               // Lambertian Diffuse
+        // point3 target = rec.p + random_in_hemisphere(rec.normal);                // Hemispherical Scattering
+        
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);    // assign colour based off of ray from point to target
     }
 
     // Sky background (gradient blue-white)
@@ -28,6 +41,7 @@ int main() {
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     hittable_list world;
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
@@ -45,7 +59,7 @@ int main() {
                 auto u = (i + random_double()) / (image_width-1);
                 auto v = (j + random_double()) / (image_height-1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
