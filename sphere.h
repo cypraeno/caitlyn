f3802a26a8fb3625e801414e1573967db8d72b1a
connortbot <loiconnor8@gmail.com>
2023-04-28 23:25:48 -0400

    Finished + Tested Chapter 9
    Haven't resolved issue with incomplete declarations of hit_record instantiations.

--- sphere.h
+++ sphere.h
@@ -1,8 +1,8 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
+#include "general.h"
 #include "hittable.h"
-#include "vec3.h"
 
 
 class sphere : public hittable {
--- vec3.h
+++ vec3.h
@@ -5,6 +5,7 @@
 #include <iostream>
 
 using std::sqrt;
+using std::fabs;
 
 
 
@@ -50,6 +51,11 @@ class vec3 {
 
         inline static vec3 random(double min, double max) {
             return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+        }
+        bool near_zero() const {
+            // Return true if the vector is close to zero in all dimensions.
+            const auto s = 1e-8;
+            return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
         }
     public:
         double e[3];
@@ -119,6 +125,10 @@ vec3 random_in_hemisphere(const vec3& normal) {
         return in_unit_sphere;
     else
         return -in_unit_sphere;
+}
+
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
 }
 
 #endif
