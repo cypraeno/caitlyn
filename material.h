1e7bba7b404f3b25cbefdc36f88b849e8c50c9c4
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2023-04-30 00:18:14 -0700

    Updated up to 10.3

--- material.h
+++ material.h
@@ -61,4 +61,35 @@ class metal : public material {
         double fuzz;
 };
 
+class dielectric : public material {
+    public:
+        dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+        virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) 
+        const override {
+            attenuation = color(1.0, 1.0, 1.0);
+            double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+            vec3 unit_direction = unit_vector(r_in.direction());
+            double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+            double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+            bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+            vec3 direction;
+
+            if (cannot_refract) {
+                direction = reflect(unit_direction, rec.normal);
+            }
+            else {
+                direction = refract(unit_direction, rec.normal, refraction_ratio);
+            }
+
+            scattered = ray(rec.p, direction);
+            return true;
+        }
+    
+    public:
+        double ir;  // Index of Refraction
+};
+
 #endif
