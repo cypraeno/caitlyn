e02b6733c678eccddfeedbe05d513a524675c9aa
connortbot <loiconnor8@gmail.com>
2023-05-02 00:00:04 -0400

    Done book 1

--- vec3.h
+++ vec3.h
@@ -131,4 +131,19 @@ vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2*dot(v,n)*n;
 }
 
+vec3 refract(const vec3& uv, const vec3& n, double eta_over_etap) {
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 out_perp =  eta_over_etap * (uv + cos_theta*n);
+    vec3 out_parallel = -sqrt(fabs(1.0 - out_perp.length_squared())) * n;
+    return out_perp + out_parallel;
+}
+
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
 #endif
