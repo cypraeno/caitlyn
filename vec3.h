9654793043a7aacd9fe3302cad4b8720d908941f
connortbot <loiconnor8@gmail.com>
2023-05-09 22:01:49 -0400

    Fix main

--- vec3.h
+++ vec3.h
@@ -149,12 +149,4 @@ vec3 random_in_unit_disk() {
         return p;
     }
 }
-
-vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
-    auto cos_theta = fmin(dot(-uv, n), 1.0);
-    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
-    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
-    return r_out_perp + r_out_parallel;
-}
-
 #endif
