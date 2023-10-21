bef6c6371fc94c18fd1976c41aedc3ffea3d1942
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2023-10-21 10:50:52 -0400

    Cleaned-up + reformatted

--- hittable_list.h
+++ hittable_list.h
@@ -11,21 +11,24 @@ using std::shared_ptr;
 using std::make_shared;
 
 class hittable_list : public hittable {
+
     public:
+
         hittable_list() {}
         hittable_list(shared_ptr<hittable> object) { add(object); }
 
         void clear() { objects.clear(); }
         void add(shared_ptr<hittable> object) { objects.push_back(object); }
 
-        virtual bool hit(
-            const ray& r, double t_min, double t_max, hit_record& rec) const override;
+        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
     public:
+
         std::vector<shared_ptr<hittable>> objects;
 };
 
 bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+
     hit_record temp_rec;
     bool hit_anything = false;
     auto closest_so_far = t_max;
