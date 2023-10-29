733d4879e315638056de8ebcb9384da609157599
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2023-10-29 11:50:05 -0400

    Minor bug fix

--- vec3.h
+++ vec3.h
@@ -125,7 +125,7 @@ vec3 random_in_hemisphere(const vec3& normal) {
 
     vec3 in_unit_sphere = random_in_unit_sphere();
 
-    if (dot(in_unit_sphere, normal) > 0.0) return in_unit_sphere;`  // In the same hemisphere as the normal
+    if (dot(in_unit_sphere, normal) > 0.0) return in_unit_sphere;  // In the same hemisphere as the normal
     else return -in_unit_sphere;
 }
 
