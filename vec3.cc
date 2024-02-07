14a037661eba13a188a44acda7f45cb7eb0aa90c
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2024-02-07 02:57:28 -0500

    Fixed flaw with random_in_unit_sphere

--- vec3.cc
+++ vec3.cc
@@ -96,7 +96,11 @@ vec3 unit_vector(vec3 v) { return v / v.length(); }
 
 vec3 random_unit_vector() { return unit_vector(random_in_unit_sphere()); }
 
-vec3 random_in_unit_sphere() { return vec3::random_unit(); }
+vec3 random_in_unit_sphere() { 
+    vec3 p;
+    while (p = vec3::random(-1, 1); p.length_squared() >= 1) {}
+    return p;
+ }
 
 vec3 random_in_hemisphere(const vec3& normal) {
 
