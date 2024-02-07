08c7c5787d980024dbec975ab2c916604a4ed912
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2024-02-07 02:51:46 -0500

    Updated color.h to use Interval

--- color.h
+++ color.h
@@ -18,9 +18,10 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     g = sqrt(scale * g);
     b = sqrt(scale * b);
     
-    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
-        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
-        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
+    static const Interval intensity(0.000, 0.999);
+    out << static_cast<int>(256 * intensity.clamp(r)) << ' '
+        << static_cast<int>(256 * intensity.clamp(g)) << ' '
+        << static_cast<int>(256 * intensity.clamp(b)) << '\n';
 }
 
 #endif
