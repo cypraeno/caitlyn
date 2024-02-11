c79026a659e77c06bacad364cf4b66b69d49acf9
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2024-02-11 14:41:10 -0500

    CA-47 Revamp interval class

--- interval.h
+++ interval.h
@@ -3,7 +3,6 @@
 
 #include "vec3.h"
 
-
 using std::fmax;
 using std::fmin;
 
@@ -12,7 +11,7 @@ public:
     double min;
     double max;
     interval(): min(0), max(0) {}
-    interval(double min, double max): min(min), max(max) {}
+    interval(double _min, double _max): min(_min), max(_max) {}
     interval(const interval& a, const interval& b): min(fmin(a.min, b.min)), max(fmax(a.max, b.max)) {}
 
     double size() const { return max - min; }
@@ -21,9 +20,16 @@ public:
         auto padding = delta / 2;
         return interval(min - padding, max + padding);
     }
-    
+
+    bool contains(double x) const { return this.min <= x && x <= this.max}
+
+    bool surrounds(double x) const { return this.min < x && x < this.max }
+
+    static const interval empty, universe;
 };
 
+const static interval empty (+infinity, -infinity);
+ocnst static interval universe (-infinity, +infinity);
 
 #endif
 
