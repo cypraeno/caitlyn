155410e8e2abbf73d02ca785e7996177c3786243
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2024-02-11 15:34:18 -0500

    CA-47 Updated ray class

--- ray.h
+++ ray.h
@@ -6,11 +6,9 @@
 class ray {
     
     public:
-        point3 orig;
-        vec3 dir;
-        double tm;
 
         ray() {}
+        ray(const point3& origin, const vec3& direction) : orig{origin}, dir{direction}, tm{0} {}
         ray(const point3& origin, const vec3& direction, double time = 0.0) : orig{origin}, dir{direction}, tm{time} {}
 
         point3 origin() const  { return orig; }
@@ -18,6 +16,12 @@ class ray {
         double time() const { return tm; }
 
         point3 at(double t) const { return orig + t*dir; }
+
+    private: 
+
+        point3 orig;
+        vec3 dir;
+        double tm;
 };
 
 #endif
