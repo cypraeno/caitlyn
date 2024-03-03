a8b4c793f6cc12e2d032a6b4fcd1acca9002a354
Max Tan <mthockey84@gmail.com>
2024-03-03 14:33:02 -0500

    CA-51-embree 7.1

--- material.h
+++ material.h
@@ -10,6 +10,10 @@ class hit_record;
 class material {
 
     public:
+        virtual color emitted(double u, double v, const point3& p) const {
+            return color(0,0,0);
+        }
+
         virtual bool scatter(const ray& r_in, const HitInfo& rec, color& attenuation, ray& scattered) const = 0;
 };
 
@@ -121,6 +125,24 @@ class dielectric : public material {
 
             return r0 + (1 - r0) * pow((1 - cosine), 5);
         }
+};
+
+class diffuse_light : public material {
+  public:
+    diffuse_light(shared_ptr<texture> a) : emit(a) {}
+    diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+
+    bool scatter(const ray& r_in, const HitInfo& rec, color& attenuation, ray& scattered)
+    const override {
+        return false;
+    }
+
+    color emitted(double u, double v, const point3& p) const override {
+        return emit->value(u, v, p);
+    }
+
+  private:
+    shared_ptr<texture> emit;
 };
 
 #endif
