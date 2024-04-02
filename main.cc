c2a5b070fe8ca8e18aaef0165b81a67002a0d7d7
connortbot <loiconnor8@gmail.com>
2024-04-01 22:41:31 -0400

    update BRDF testing func

--- main.cc
+++ main.cc
@@ -927,7 +927,7 @@ void brdf_tests() {
     color test = color(1.0, 1.0, 1.0);
     auto mt = make_shared<lambertian>(test);
     auto mt2 = make_shared<OrenNayar>(test, 0.0);
-    auto mt3 = make_shared<CookTorrance>(color(0.8, 0.4, 0.4), 0.1, 1.0, 0.5);
+    auto mt3 = make_shared<CookTorrance>(color(1.0, 1.0, 1.0), 0.01, 0.0, 0.0);
 
     //auto mt = make_shared<dielectric>(1.5);
     auto sphere1 = make_shared<SpherePrimitive>(point3(0, 2, 2), mt, 2, device);
@@ -943,10 +943,6 @@ void brdf_tests() {
     rtcReleaseDevice(device);
 
     output(render_data, cam, scene_ptr);
-
-    std::cout << mt3->random_GGX_microfacet(vec3(0,1,0)) << std::endl;
-    std::cout << mt3->random_GGX_microfacet(vec3(0,1,0)) << std::endl;
-    std::cout << mt3->random_GGX_microfacet(vec3(0,1,0)) << std::endl;
 }
 
 int main(int argc, char* argv[]) {
