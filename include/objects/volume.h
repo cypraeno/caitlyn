21b451f6f67e1c9aff628f7f4b277e9d571a9fe4
connortbot <loiconnor8@gmail.com>
2024-05-03 12:22:49 -0400

    CA-120 volume object built from a medium and a geometry object to copy the geometry of

---
+++ include/objects/volume.h
@@ -0,0 +1,23 @@
+#ifndef VOLUME_H
+#define VOLUME_H
+
+#include "geometry.h"
+#include "medium.h"
+
+class Volume : public Geometry {
+    public:
+    // Releases geometry pointer of the geom ptr, and keeps for itself. Thus, the used geometry cannot be added itself.
+    Volume(shared_ptr<Medium> medium, shared_ptr<Geometry> geom);
+
+    virtual shared_ptr<material> materialById(unsigned int geomID) const override;
+
+    virtual HitInfo getHitInfo(const ray& r, const vec3& p, const float t, unsigned int geomID) const override;
+
+    // sample and pdf are not implemented as volumes should not be sampled for direct light sampling
+    // however, reconsider with emissive volumes
+
+    shared_ptr<Medium> medium;
+    shared_ptr<Geometry> geom_ptr;
+};
+
+#endif
