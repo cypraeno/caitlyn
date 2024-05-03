f4203892443dfa02d59fc0a42747875fbb0088b7
connortbot <loiconnor8@gmail.com>
2024-05-03 12:16:02 -0400

    CA-120 track if the hit object is a medium or not, defaults to false

--- include/intersection/hitinfo.h
+++ include/intersection/hitinfo.h
@@ -12,6 +12,8 @@ struct HitInfo {
     double u;
     double v;
 
+    bool medium = false;
+
     /** @brief Given a face's outward normal and the initial ray, sets front_face to represent
     if collision hits it from the front or not. */
     inline void set_face_normal(const ray& r, const vec3& outward_normal) {
