5df4d47d60200d0c38f1d167cf1d920b393fa9e4
connortbot <loiconnor8@gmail.com>
2024-05-07 22:31:43 -0400

    CA-120 basic transmittance for Mediums

--- include/materials/medium.h
+++ include/materials/medium.h
@@ -1,6 +1,7 @@
 #ifndef MEDIUM_H
 #define MEDIUM_H
 
+#include "general.h"
 #include "material.h"
 
 class Medium {
@@ -8,6 +9,14 @@ class Medium {
     Medium(float density, shared_ptr<material> phase_function);
 
     virtual float particleDistance() const;
+
+    /**
+     * @brief calculates transmittance coefficient between point x and y, assuming a constant medium.
+     * @note Assumes density as equivalent to extinction coefficient.
+     * May not be accurate!
+    */
+    virtual float transmittance(const vec3& x, const vec3& y) const;
+    virtual float transmittance(float dist) const;
 
     float density;
     shared_ptr<material> phase;
--- src/materials/medium.cc
+++ src/materials/medium.cc
@@ -8,3 +8,15 @@ float Medium::particleDistance() const {
     auto hit_distance = neg_inv_density * log(random_double());
     return hit_distance;
 }
+
+float Medium::transmittance(const vec3& x, const vec3& y) const {
+    float exponent = -(density * fabs((x - y).length()));
+    return pow(euler, exponent);
+}
+
+float Medium::transmittance(float dist) const {
+    float exponent = -(density * dist);
+    return pow(euler, exponent);
+}
+
+}
