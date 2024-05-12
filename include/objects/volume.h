79df51ee6668783ea7875576d9a1a0d0c6644c5b
connortbot <loiconnor8@gmail.com>
2024-05-12 13:38:43 -0400

    CA-120 make internal scene for Volumes for isolated checks if we are inside of them

--- include/objects/volume.h
+++ include/objects/volume.h
@@ -7,7 +7,7 @@
 class Volume : public Geometry {
     public:
     // Releases geometry pointer of the geom ptr, and keeps for itself. Thus, the used geometry cannot be added itself.
-    Volume(shared_ptr<Medium> medium, shared_ptr<Geometry> geom);
+    Volume(shared_ptr<Medium> medium, shared_ptr<Geometry> geom, RTCDevice device);
 
     virtual shared_ptr<material> materialById(unsigned int geomID) const override;
 
@@ -16,6 +16,7 @@ class Volume : public Geometry {
     // sample and pdf are not implemented as volumes should not be sampled for direct light sampling
     // however, reconsider with emissive volumes
 
+    RTCScene volume_scene; // object scene only containing the volume
     shared_ptr<Medium> medium;
     shared_ptr<Geometry> geom_ptr;
 };
--- src/objects/volume.cc
+++ src/objects/volume.cc
@@ -1,6 +1,11 @@
 #include "volume.h"
 
-Volume::Volume(shared_ptr<Medium> medium, shared_ptr<Geometry> geom_ptr) : medium{medium}, geom_ptr{geom_ptr}, Geometry(geom_ptr) {}
+Volume::Volume(shared_ptr<Medium> medium, shared_ptr<Geometry> geom_ptr, RTCDevice device) : medium{medium}, geom_ptr{geom_ptr}, Geometry(geom_ptr) {
+    volume_scene = rtcNewScene(device);
+    unsigned int geomID = rtcAttachGeometry(volume_scene, geom_ptr->geom);
+    rtcReleaseGeometry(geom_ptr->geom);
+    rtcCommitScene(volume_scene);
+}
 
 shared_ptr<material> Volume::materialById(unsigned int geomID) const {
     return medium->phase;
