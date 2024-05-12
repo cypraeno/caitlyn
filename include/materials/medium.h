db8fa9ee0a5e29d5a7038b406db6ca10fa370963
connortbot <loiconnor8@gmail.com>
2024-05-12 13:38:17 -0400

    CA-120 MediumRecord class for maintaining residing mediums and transmittance in traced ray paths

--- include/materials/medium.h
+++ include/materials/medium.h
@@ -1,6 +1,8 @@
 #ifndef MEDIUM_H
 #define MEDIUM_H
 
+#include <algorithm>
+#include <map>
 #include "general.h"
 #include "material.h"
 
@@ -20,6 +22,51 @@ class Medium {
 
     float density;
     shared_ptr<material> phase;
+};
+
+
+/**
+ * @class MediumRecord
+ * @brief Used for two purposes: tracking of mediums when ray tracing/marching and for calculating transmittance in DLS.
+*/
+class MediumRecord {
+    public:
+    std::vector<shared_ptr<Medium>> mediums;
+    
+    // Transmittance calculation variables
+    float transmittance = 1.0;
+    point3 recent_position;
+    shared_ptr<Medium> highest_density_volume = nullptr;
+
+
+
+    MediumRecord(point3 initial_position);
+    
+    bool contains(std::shared_ptr<Medium> vol) const;
+    
+    /**
+     * @brief Used to update the mediums list and update transmittance.
+     * Called whenever a new volume is hit in which we travel through the participating mediums.
+     * 
+     * 1. Let D be the distance between new point and previous point. This is distance traveled.
+     * 2. Multiply transmittance by the transmittance applied by the highest_density_volume in D distance.
+     * 3A. If volume is in mediums, then we are exiting - remove from mediums.
+     * 3B. If volume is NOT in mediums, then we are entering. Check if vol->density > highest_density_volume->density.
+    */
+    void hitVolume(shared_ptr<Medium> vol, point3 hitPoint);
+
+    /**
+     * @brief Only used for direct light sampling. Given
+    */
+
+    /**
+     * @brief Iterates through mediums and calls particleDistance to simulate the nearest particle of all
+     * participating mediums.
+     * @returns ptr to the particle's corresponding medium and updates passed in dist float.
+    */
+    shared_ptr<Medium> particleDistance(float& dist) const;
+
+    
 };
 
 
--- src/materials/medium.cc
+++ src/materials/medium.cc
@@ -19,4 +19,59 @@ float Medium::transmittance(float dist) const {
     return pow(euler, exponent);
 }
 
+
+// MediumStack
+
+MediumRecord::MediumRecord(point3 initial_position) : recent_position{initial_position} {}
+
+bool MediumRecord::contains(std::shared_ptr<Medium> vol) const {
+    return std::find(mediums.begin(), mediums.end(), vol) != mediums.end();
+}
+
+void MediumRecord::hitVolume(shared_ptr<Medium> vol, point3 hitPoint) {
+    float distance_traveled = (recent_position - hitPoint).length();
+    if (!mediums.empty()) {
+        transmittance *= highest_density_volume->transmittance(distance_traveled);
+        if (!vol) { return; }
+        if (!contains(vol)) { // entering
+            mediums.push_back(vol);
+            if (vol->density > highest_density_volume->density) { highest_density_volume = vol; }
+        } else { // exiting
+            mediums.erase(std::remove(mediums.begin(), mediums.end(), vol), mediums.end());
+
+            float highestDensity = -1.0f;
+            if (vol == highest_density_volume) { // reset highest density volume by finding it
+                for (const auto& ptr : mediums) {
+                    if (ptr->density > highestDensity) {
+                        highestDensity = ptr->density;
+                        highest_density_volume = ptr;
+                    }
+                }
+            }
+        }
+    } else { // mediums empty
+        if (!vol) { return; }
+        highest_density_volume = vol;
+        mediums.push_back(vol);
+        recent_position = hitPoint;
+    }
+}
+
+shared_ptr<Medium> MediumRecord::particleDistance(float& dist) const {
+    float min_dist;
+    shared_ptr<Medium> p = nullptr;
+    for (size_t i = 0; i < mediums.size(); ++i) {
+        float ndist = mediums[i]->particleDistance();
+        if (i == 0) {
+            min_dist = ndist;
+            p = mediums[i];
+        } else {
+            if (ndist < min_dist) {
+                min_dist = ndist;
+                p = mediums[i];
+            }
+        }
+    }
+    dist = min_dist;
+    return p;
 }
