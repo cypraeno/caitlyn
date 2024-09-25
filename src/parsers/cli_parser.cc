1102c4026903c5462aeb80cf87bfe80387aa8e0e
connortbot <loiconnor8@gmail.com>
2024-09-24 23:37:31 -0400

    CA-89 change version to 0.1.5

--- src/parsers/cli_parser.cc
+++ src/parsers/cli_parser.cc
@@ -109,7 +109,7 @@ Config parseArguments(int argc, char* argv[]) {
 
         else if(arg == "-v" || arg == "--version") {
             config.showVersion = true;
-            std::cout << "caitlyn version 0.1.3" << std::endl;
+            std::cout << "caitlyn version 0.1.5" << std::endl;
         } 
         
         else if(arg == "-h" || arg == "--help") {
